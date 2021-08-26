62c55485614984e20c15956d9fa1777428da6b4f
schusmeicer <59938696+schusmeicer@users.noreply.github.com>
2021-08-25 21:42:29 -0300

    finalização do projeto arranjo, criação dos testes!

--- src/Arranjo.java
+++ src/Arranjo.java
@@ -4,10 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Arranjo {
     @Test
-     public static void main(String[] args) {
+     void test() {
          int[] b = {940, 880, 830, 790, 750, 660, 650, 590, 510, 940};
          Op a = new Op();
          assertEquals(510, a.menor(b), "O Menor deve ser 510");
+         assertEquals(940, a.maior(b), "O maior é 940");
+         assertEquals(7540, a.soma(b), "A soma é 7540");
+         assertEquals(0, a.repitacaoArray(b,3), "3 aparece 0 vezes");
+         assertEquals(1, a.repitacaoArray(b,790), "790 aparece 1 vezes");
+         assertEquals(2, a.repitacaoArray(b,940), "940 aparece 2 vezes");
 
     }
 
